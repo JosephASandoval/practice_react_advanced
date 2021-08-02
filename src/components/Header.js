7960dcf13d2fb3693dc7e89a528bac5433735560
JosephASandoval <josephasandoval17@gmail.com>
2021-08-01 18:45:24 -0700

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,10 @@ import { Context } from "../Context";
 
 function Header() {
   const { cartItems } = useContext(Context);
+
   const cartClassName =
     cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line";
+
   return (
     <header>
       <Link to="/">
